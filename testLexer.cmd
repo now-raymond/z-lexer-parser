44f21d8e7743bfe9d5f731e43949901304539c2c
now-raymond <raymond.tan@live.com>
2017-03-04 01:19:27 +0000

    Add Windows helpers

---
+++ testLexer.cmd
@@ -0,0 +1,3 @@
+@echo off
+REM This file tests the lexer on Windows.
+java -cp bin;lib/java-cup-11b-runtime.jar Lexer tests/open/p-imply.s
---
+++ testSystem.cmd
@@ -0,0 +1,3 @@
+@echo off
+REM This file runs a test case through the lexer+scanner on Windows.
+java -cp bin;lib/java-cup-11b-runtime.jar SC_Verbose tests/open/p-imply.s
