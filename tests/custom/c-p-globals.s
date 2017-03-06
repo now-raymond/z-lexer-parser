821b66d522b95c4ec34d066a7f30e77403a465d6
now-raymond <rayjta@gmail.com>
2017-03-06 10:40:12 +0000

    Add custom tests for reserved keywords, globals, and rats

---
+++ tests/custom/c-p-globals.s
@@ -0,0 +1,15 @@
+globalVar:rat := null;
+anotherVar:float := 3;
+
+main { 
+  #some comment
+  nullVar:int := null;
+  
+  # Do some stuff
+  a[3] := 9999;
+  
+  # anotherVar
+  #another:int := null;
+};
+
+anotherGlobalVar:float := 3;
