3083e235ce9b5a662705ed0dacdbe2cdcb958e3d
now-raymond <rayjta@gmail.com>
2017-03-07 19:49:29 +0000

    char and global test cases custom

---
+++ tests/custom/c-p-char.s
@@ -0,0 +1,6 @@
+main { 
+  myChar:char := 'a';
+  #myInvalidChar:char := 'ʖ';
+  myStr:string := "HELLO World!";
+  myStruct:person := "Shin", "Yoo", 30;
+};
--- tests/custom/c-p-globals.s
+++ tests/custom/c-p-globals.s
@@ -2,14 +2,18 @@ globalVar:rat := null;
 anotherVar:float := 3;
 
 main { 
-  #some comment
+  # declaration
   nullVar:int := null;
   
   # Do some stuff
   a[3] := 9999;
   
-  # anotherVar
+  # a var declaration that isn't at the top
   #another:int := null;
+  
+  # Some action
+  #print 2 in [3,5,6];
+  
 };
 
 anotherGlobalVar:float := 3;
